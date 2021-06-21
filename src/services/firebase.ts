41166d822e3e670a87ba46b12b7422ce0dea86e2
Dimas Resende <dresende@uolinc.com>
2021-06-21 00:10:29 -0300

    Alteracao da config para variavel de ambiente

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,13 +3,13 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBazu6m00fV11ZCjORHz_34usN4VRU-NrI",
-  authDomain: "letmeask-d9a38.firebaseapp.com",
-  databaseURL: "https://letmeask-d9a38-default-rtdb.firebaseio.com",
-  projectId: "letmeask-d9a38",
-  storageBucket: "letmeask-d9a38.appspot.com",
-  messagingSenderId: "197319196167",
-  appId: "1:197319196167:web:b31f515e41c7645aa9b4a2"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
