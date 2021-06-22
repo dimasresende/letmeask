be4c71f60157b7c760acbf582cbce082b3f14dc2
Dimas Resende <dresende@uolinc.com>
2021-06-21 23:12:14 -0300

    Encapsulando o Context

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ import googleIconImg from '../assets/images/google-icon.svg';
 import { Button } from '../components/Button';
 
 import '../styles/auth.scss';
-import { AuthContext } from '../App';
+import { AuthContext } from '../contexts/AuthContext';
 
 export function Home() {
   const history = useHistory();
