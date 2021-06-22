eef89e18b419299e2d7c7d8003fe75afc3995724
Dimas Resende <dresende@uolinc.com>
2021-06-21 23:16:29 -0300

    Criando hook de autenticacao

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,16 +1,15 @@
 import { Link } from 'react-router-dom';
-// import { useContext } from 'react';
-// import { AuthContext } from '../contexts/AuthContext';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 
 import { Button } from '../components/Button';
+// import { useAuth } from '../hooks/useAuth';
 
 import '../styles/auth.scss';
 
 export function NewRoom() {
-  // const { user } = useContext(AuthContext);
+  // const { user } = useAuth();
 
   return (
     <div id="page-auth">
